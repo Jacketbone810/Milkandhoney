531442928eaa304f39b3eb4b386956aae370fcb0
Jake <150399292+Jacketbone810@users.noreply.github.com>
2024-03-17 18:02:24 -0400

    Create js interactivity and update matt picture

---
+++ friends.js
@@ -0,0 +1,13 @@
+let matt = document.querySelector('#Matt');
+let container = document.querySelector('#containerRow');
+let p = document.querySelector('#placeholder');
+
+matt.addEventListener('click', function(){
+    p.textContent='';
+    p.textContent='While Guar\'s father was dying he gave \
+    him a book that talks about a prophecy about this ancient weapon\
+    that will on day stop evil events from happening in the wworld!\
+    His father siad that weapon used to be in their family for generations\
+    but got stolen and probably stolen around the world. noe he\'s going \
+    to go get it back!';
+})
