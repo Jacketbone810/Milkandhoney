b682297732ae5e4074a6d3057297f24301270e3a
Jake <150399292+Jacketbone810@users.noreply.github.com>
2024-03-17 19:00:12 -0400

    Add interactive joey option and hide and show for img

--- friends.js
+++ friends.js
@@ -1,13 +1,26 @@
-let matt = document.querySelector('#Matt');
-let container = document.querySelector('#containerRow');
-let p = document.querySelector('#placeholder');
 
-matt.addEventListener('click', function(){
-    p.textContent='';
-    p.textContent='While Guar\'s father was dying he gave \
-    him a book that talks about a prophecy about this ancient weapon\
-    that will on day stop evil events from happening in the wworld!\
-    His father siad that weapon used to be in their family for generations\
-    but got stolen and probably stolen around the world. noe he\'s going \
-    to go get it back!';
-})
+    let matt = document.querySelector('#Matt');
+    let joey = document.querySelector('#Joey');
+    let container = document.querySelector('#inner');
+    let p = document.querySelector('#placeholder');
+    let img = document.createElement('img');
+    
+    matt.addEventListener('click', function(){
+        img.style.display='none';
+        container.style.flexDirection='row';
+        p.textContent='';
+        p.textContent='While Guar\'s father was dying he gave \
+        him a book that talks about a prophecy about this ancient weapon\
+        that will on day stop evil events from happening in the wworld!\
+        His father siad that weapon used to be in their family for generations\
+        but got stolen and probably stolen around the world. noe he\'s going \
+        to go get it back!';
+    })
+    
+    joey.addEventListener('click', function(){
+        img.style.display='inline';
+        container.style.flexDirection='column';
+        p.textContent='He never sent me anything....';
+        img.src='./ImACop.jpg';
+        container.appendChild(img);
+    })
